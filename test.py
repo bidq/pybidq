465fad8a30d41be1cdf6f9bd871691605932baea
Alon Niv <oakfang@gmail.com>
2019-11-03 13:34:40 +0200

    Initial code

---
+++ test.py
@@ -0,0 +1,26 @@
+import asyncio
+from bidq import BidQ
+
+
+async def setup_worker():
+    client = await BidQ.create()
+
+    @client.worker("add")
+    def add(x, y):
+        return x + y
+
+    await client.serve()
+
+
+async def main():
+    client = await BidQ.create()
+    jid = await client.submit("add", {"x": 1, "y": 2}, 0.2)
+    try:
+        print(await client.get(jid))
+    except Exception as e:
+        print(e)
+    await client.close()
+
+
+# asyncio.run(main())
+asyncio.run(setup_worker())
